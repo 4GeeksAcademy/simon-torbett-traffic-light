d427d1a8ce7cf56a547e6f63e4ab31cde3403802
Storbett21 <96504042+Storbett21@users.noreply.github.com>
2023-09-29 20:49:52 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,6 +6,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const  [selector, setselector] = useState ("luz amarilla")
+	
+	const cambiarColor = () => {
+		if (selector === "luz roja") {
+		  setselector("luz amarilla");
+		} else if (selector === "luz amarilla") {
+		  setselector("luz verde");
+		} else {
+		  setselector("luz roja");
+		}
+	  };
 	return (
 		<div>
 		<div id="barra"></div>
@@ -16,6 +26,7 @@ const Home = () => {
 		  <div onClick={()=> setselector("luz amarilla")} className= {"luz amarilla" + (selector === "luz amarilla" ? " brillo" : "")}></div>
 		  <div onClick={()=> setselector("luz verde")} className= {"luz verde" + (selector === "luz verde" ? " brillo" : "")}></div>
 		</div>
+		<button onClick={() => cambiarColor()}>Cambiar Color</button>
 		</div>
 	  
 	  );
